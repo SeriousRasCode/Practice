8c2088c3aaa4522959dbf6ffa01b6fc88669b1b4
TedrHa <Tedrhanna@gmail.com>
2024-11-09 20:46:30 +0300

    Add Icon Add Js but not used yet.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-function displayText() {
-    var text = document.getElementById("textField");
-    text.style.display = "block";
-  }
-  
-  
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
